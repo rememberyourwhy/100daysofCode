b7767b85dacacdc46c2957f063ac7587b9c4768b
tp <phuc16052001@gmail.com>
2022-08-09 10:42:54 +0700

    jsdlfajdlf

---
+++ Day 19/expand_on_turtle_race/turtle_race.py
@@ -0,0 +1,27 @@
+# from turtle import Turtle, Screen
+# import random
+
+# What will be different here
+# I'll create some obstacle on the race to slow turtles down or boost them
+# How to do it?
+# Go through an x-axis and put obstacles and boosts on it. (step = 5)
+# Save these coordinates to a list
+# TC, SC
+
+# Change the way the turtles move, so they can hit obstacles or boosts easier
+# Move 5 or 10 each time
+
+
+# Create screen object, change their setup(width, height)
+# Create color list
+# Create y_coordinate list
+# Create turtle objects, store them inside a list
+# Move them to their right position
+# Make a bet
+# If the bet is made
+# Start the run inside a while loop
+# Move each turtle in the list by random distance, 5 or 10
+# Check if they run into an obstacle ( -5 distance ) or a boost ( +10 distance )
+# Check if they finished the race and print result
+
+# Todo: Check if you want to do this
---
+++ Day 20/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-60, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for turtle_index in range(3):
+            # create turtle object while square shape and white color
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            # pu and set x_coordinate
+            new_segment.pu()
+            new_segment.setpos(STARTING_POSITIONS[turtle_index])  # why setting constant --> easier to modify later
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x=new_x, y=new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
