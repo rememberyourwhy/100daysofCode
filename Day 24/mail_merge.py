1dc6a85145d9d9aeab36a799cc982c58f94d0c72
tp <phuc16052001@gmail.com>
2022-08-12 18:37:59 +0700

    working with files

---
+++ Day 24/mail_merge.py
@@ -0,0 +1,23 @@
+# Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+    
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open("Input/Letters/starting_letter.txt", mode="r") as sample_letter_file:
+    sample_letter = sample_letter_file.read()
+
+with open("Input/Names/invited_names.txt", mode="r") as invited_names:
+    name_str = invited_names.read()
+    name_list = name_str.split("\n")
+
+for name in name_list:
+    temp_letter = sample_letter
+    temp_letter = temp_letter.replace("[name]", name)
+    path = f"Output/ReadyToSend/letter_for_{name}.txt"
+    with open(path, mode="w") as finished_output:
+        finished_output.write(temp_letter)
