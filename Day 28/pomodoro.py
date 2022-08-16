8b829413322d79ec71938044e4d73919f2d96fb9
tp <phuc16052001@gmail.com>
2022-08-16 15:35:36 +0700

    Done coding day 28 project on my own. Now it's time to eat humiliation

--- Day 28/pomodoro.py
+++ Day 28/pomodoro.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import time
 
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -16,9 +15,21 @@ CHECK_MARK = "✓"
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global cancel_id
+    if cancel_id is not None:
+        window.after_cancel(cancel_id)
+        cancel_id = None
+        buttons[0].config(state="active")
+        canvas.itemconfig(timer_text, text="00:00")
+        check_mark_label_var.set("")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
+    if True:
+        buttons[0].config(state="disabled")
+        print("bru")
     timer_label_var.set("Work")
     count_down(WORK_MIN * 60)
 
@@ -26,6 +37,7 @@ def start_timer():
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count, break_time=False):
     global WORK_MIN, SHORT_BREAK_MIN, LONG_BREAK_MIN
+    global cancel_id
 
     # Make timer looks nice
     time_str = f"{(count // 60):02}:{(count % 60):02}"
@@ -33,21 +45,24 @@ def count_down(count, break_time=False):
 
     # Check condition
     if count > 0:
-        window.after(1000, count_down, count - 1, break_time)
+        cancel_id = window.after(1000, count_down, count - 1, break_time)
+    # End of working session, break_time start soon
     elif count == 0 and not break_time:
         current = check_mark_label_var.get()
         check_mark_label_var.set(current + "✓")
-        if current == "✓✓✓":
+        if current == "✓✓✓":  # 4th time but since current haven't updated itself.
             timer_label_var.set("Long Break")
             count_down(LONG_BREAK_MIN * 60, break_time=True)
-            check_mark_label_var.set("")
-        else:
+        else:  # not 4th time
             timer_label_var.set("Break")
             count_down(SHORT_BREAK_MIN * 60, break_time=True)
+    # End of break_time, going to start working session
     elif count == 0 and break_time:
+        current = check_mark_label_var.get()
+        if current == "✓✓✓✓":  # 4th time, going to reset check mark label
+            check_mark_label_var.set("")
         timer_label_var.set("Work")
         count_down(WORK_MIN * 60, break_time=False)
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -76,17 +91,14 @@ check_mark_label = Label(textvariable=check_mark_label_var, fg=ELECTRIC_GREEN, b
 check_mark_label.grid(column=2, row=4)
 
 
-def clicked_start():
-    pass
-
-
-def clicked_reset():
-    pass
-
-
 # Button
+cancel_id = None
 start_button = Button(text="Start", command=start_timer)
-reset_button = Button(text="Reset", command=clicked_reset)
+reset_button = Button(text="Reset", command=reset_timer)
+
+# Create button widgets collections and save the two button
+buttons = [start_button, reset_button]
+
 start_button.grid(column=1, row=3)
 reset_button.grid(column=3, row=3)
 window.mainloop()
