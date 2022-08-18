2b80978cf04ed0f4c4e894010a4c24d723b009e5
rememberyourwhy <phuc16052001@gmail.com>
2022-08-18 10:42:52 +0700

    finishing

--- Day 29/password_manager_upgrading/password_manager_try_except.py
+++ Day 29/password_manager_upgrading/password_manager_try_except.py
@@ -5,6 +5,27 @@ import pyperclip
 import json
 
 
+# --------------------------- SEARCH FOR WEBSITE INF ---------------------------- #
+def search_for_website():
+    website_to_search = website_entry.get()
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops", message="Data file is missing")
+    else:
+        if website_to_search in data:
+            email = data[website_to_search]["email"]
+            password = data[website_to_search]["password"]
+            messagebox.showinfo(title=website_to_search, message=f"""
+                "These are the details information for your account on this website \n"
+                "Email: {email} \n"
+                "Password: {password}"
+            """)
+        else:
+            messagebox.showinfo(title="Not added", message="Account information for this website isn't added")
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
 def generate_password():
@@ -78,8 +99,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -88,6 +109,7 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", command=search_for_website)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
