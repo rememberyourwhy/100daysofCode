71cb1030ab6206b944e512dbf8bb52827a948497
rememberyourwhy <phuc16052001@gmail.com>
2022-08-18 11:01:06 +0700

    finishing

--- Day 29/password_manager_upgrading/password_manager_try_except.py
+++ Day 29/password_manager_upgrading/password_manager_try_except.py
@@ -65,14 +65,14 @@ def save():
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
         try:
-            with open("../password_manager_upgrading/data.txt", "r") as data_file:
+            with open("data.json", "r") as data_file:
                 data = json.load(data_file)
         except FileNotFoundError:
-            with open("../password_manager_upgrading/data.txt", "w") as data_file:
+            with open("data.json", "w") as data_file:
                 json.dump(new_data, data_file, indent=4)
         else:
             data.update(new_data)
-            with open("../password_manager_upgrading/data.txt", "w") as data_file:
+            with open("data.json", "w") as data_file:
                 json.dump(data, data_file, indent=4)
         finally:
             website_entry.delete(0, END)
@@ -100,20 +100,20 @@ password_label.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry(width=21)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, sticky="ew")
 website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
 email_entry.insert(0, "angela@gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=3, column=1, sticky="ew")
 
 # Buttons
 search_button = Button(text="Search", command=search_for_website)
-search_button.grid(row=1, column=2)
+search_button.grid(row=1, column=2, sticky="ew")
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=2, sticky="ew")
 
 window.mainloop()
