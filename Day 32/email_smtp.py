b7e54517686b2182d05a05932d5852f656986fef
tp <phuc16052001@gmail.com>
2022-08-24 10:50:01 +0700

    bad networkconnection bru

---
+++ Day 32/email_smtp.py
@@ -0,0 +1,9 @@
+import smtplib
+
+# declare my_email, password
+
+# assign connection = smtplib.smtp("smtp.gmail.com")
+# starttls()
+# login (username, password)
+# sendmail (from_addr, to_addrs, msg)
+# close connection
