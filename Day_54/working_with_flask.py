91c74458d9619385eb5b39495934e31dabbf14b2
tp <phuc16052001@gmail.com>
2022-09-08 16:27:31 +0700

    Day 54

--- Day_54/working_with_flask.py
+++ Day_54/working_with_flask.py
@@ -17,3 +17,17 @@ if __name__ == "__main__":
 # you can run the file
 # without having to add FLASK_APP env variable and deal with a bunch of other things
 
+
+# -------------- ERROR 1 ------------ #
+# ERROR:
+# ps1 cannot be loaded because running scripts is disabled on this system
+
+# SOLUTION
+# This could be due to the current user having an undefined ExecutionPolicy.
+#
+# In PowerShell as Administrator, you could try the following:
+#
+# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+
+# SOURCE:
+# https://stackoverflow.com/questions/41117421/ps1-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system/49112322#49112322
